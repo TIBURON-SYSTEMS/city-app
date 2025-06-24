f3922d39635dc887c94236294231b16f0292db4b
Arnaud <141914477+arobri67@users.noreply.github.com>
2025-06-24 10:16:11 +0200

    create expo app with UI kit and React Query (#1)

---
+++ apps/App/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text } from "@/components/ui/text";
+import { View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-green-600 text-5xl">Hello</Text>
+    </View>
+  );
+}
